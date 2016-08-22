f01998a95b8af3196949b5075ff4e4e6b6f63b20
Paul Aranas <paul.aranas@ymail.com>
2016-08-21 19:55:18 -0700

    feat : Add MainActivity, NewsFeedActivity, and AsyncNewsFetcher

---
+++ app/src/main/java/ileducationgroup/org/toparmsdealers/AsyncNewsFetcher.java
@@ -0,0 +1,15 @@
+package ileducationgroup.org.toparmsdealers;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Paul on 8/21/2016.
+ */
+public class AsyncNewsFetcher extends AsyncTask<Void, Void, Void> {
+ 
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+}
