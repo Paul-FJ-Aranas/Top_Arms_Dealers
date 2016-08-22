1bbf2accd33cb65999beaa336f24eed696ff96fc
Paul Aranas <paul.aranas@ymail.com>
2016-08-21 20:44:54 -0700

    feat: Add AsyncTask for news feed

--- app/src/main/java/ileducationgroup/org/toparmsdealers/AsyncNewsFetcher.java
+++ app/src/main/java/ileducationgroup/org/toparmsdealers/AsyncNewsFetcher.java
@@ -6,7 +6,7 @@ import android.os.AsyncTask;
  * Created by Paul on 8/21/2016.
  */
 public class AsyncNewsFetcher extends AsyncTask<Void, Void, Void> {
- 
+
 
     @Override
     protected Void doInBackground(Void... voids) {
